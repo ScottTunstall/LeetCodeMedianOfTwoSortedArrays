5ac8b91534f33e7cf58a23b7ef63da3be57ddb02
Scott Tunstall <scott.tunstall@ntlworld.com>
2021-08-08 16:41:07 +0100

    Can't believe how easy this was! Even if I hadn't used LINQ this would have been easy. Is this meant to be a HARD problem?

---
+++ LeetCodeMedianOfTwoSortedArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LeetCodeMedianOfTwoSortedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            double answer = solution.FindMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 });
+            Debug.Assert(answer == 2);
+
+            answer = solution.FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 });
+            Debug.Assert(answer == 2.5);
+
+        }
+    }
+}
---
+++ LeetCodeMedianOfTwoSortedArrays/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeMedianOfTwoSortedArrays
+{
+    public class Solution
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var nums1AsList = new List<int>(nums1);
+            var nums2AsList = new List<int>(nums2);
+            nums1AsList.AddRange(nums2AsList);
+            nums1AsList.Sort();
+
+            var count = nums1AsList.Count;
+            if ((count & 1) !=0)
+            {
+                return nums1AsList[(count / 2)];
+            }
+
+            var index = (count / 2) - 1;
+            return ((double)nums1AsList[index] + (double)nums1AsList[index+1])/2;
+        }
+    }
+}
